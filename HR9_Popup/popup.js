c76912a89aa8db4137ce5de0afd3ac78fb416fe9
Lorraine-Lyu <llyu@smith.edu>
2019-09-21 21:42:58 -0500

    revert

--- HR9_Popup/popup.js
+++ HR9_Popup/popup.js
@@ -4,10 +4,14 @@ var time = 0;
 var id_W_site = 0;
 var id_B_site = 0;
 
-// var port = chrome.extension.connect({
-//     name: "Popup -> Background"
-// });
-// port.postMessage("Hi BackGround");
+document.getElementById("addWhite").addEventListener("click", addWFunction);
+document.getElementById("addBlack").addEventListener("click", addBFunction);
+
+var port = chrome.extension.connect({
+    name: "Popup -> Background"
+});
+port.postMessage("Hi BackGround");
+
 // port.onMessage.addListener(function(msg) {
 //     console.log("message recieved" + msg);
 // });
@@ -21,12 +25,10 @@ function addWFunction(){
     var text = document.getElementById("whiteList").value;
     whiteList.push(text);
     var li = document.createElement('li');
-    li.innerHTML = "<div> <label>" + text+ "</label> <input class='DeleteThis' type='button' value='Delete' /> </div>";
+    li.innerHTML = "<div> <label>" + text+ "</label> <input class='DeleteThis' type='button' value='-' /> </div>";
     document.getElementById("dsplyWhite").appendChild(li);
 
     deleteChild();
-
-
     // chrome.storage.sync.set({'id_W_site': text}, function(){
     //     console.log("URL: " + text + "is saved.");
     // });
@@ -38,7 +40,7 @@ function addBFunction(){
     var text = document.getElementById("BlackList").value;
     blackList.push(text);
     var li = document.createElement('li');
-    li.innerHTML = "<div> <label>" + text+ "</label> <input class='DeleteThis' type='button' value='Delete' /> </div>";
+    li.innerHTML = "<div> <label>" + text+ "</label> <input class='DeleteThis' type='button' value='-' /> </div>";
     document.getElementById("dsplyBlack").appendChild(li);
 
     deleteChild();
